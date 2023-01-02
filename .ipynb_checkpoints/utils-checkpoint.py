613c8722f9948829fc28b8867b47e13cbaa96f6a
Lyssa <emfreese@mit.edu>
2023-01-02 17:30:21 -0500

    cdr pulse added for GF, fixed 1pc

--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -14,6 +14,7 @@ import xmip.drift_removal as xm_dr
 import xmip as xm
 import xesmf as xe
 import datetime
+from datetime import timedelta
 from dateutil.relativedelta import relativedelta
 
 
@@ -22,7 +23,7 @@ from dateutil.relativedelta import relativedelta
 #### initial import and data merging ####
 #subroutines to import
 
-def _import_combine_pulse_control(control_path, pulse_path, replace_xy):
+def _import_combine_pulse_control(control_path, pulse_path, replace_xy, m):
     #import and check files
     if replace_xy == True:
         ds_control = xr.open_mfdataset(control_path, preprocess = combined_preprocessing, use_cftime=True)
@@ -32,6 +33,11 @@ def _import_combine_pulse_control(control_path, pulse_path, replace_xy):
         ds_pulse = xr.open_mfdataset(pulse_path, use_cftime=True)
     if ds_control.attrs['parent_source_id'] != ds_pulse.attrs['parent_source_id']:
         print('WARNING: Control and Pulse runs are not from the same parent source!')
+    #fix the time for two of the models
+    if m == 'NORESM2':
+        ds_pulse['time'] = ds_control['time'][:len(ds_pulse['time'])]
+    if m =='CANESM5_r1p2':
+        ds_pulse['time'] = ds_control['time'][:len(ds_pulse['time'])]
     #select only the times that match up with the pulse
     ds_control = ds_control.sel(time = slice(ds_control['time'].min(), ds_pulse['time'].max()))
     #fix the lat lon/xy gridding (xmip)
@@ -44,19 +50,33 @@ def _import_combine_pulse_control(control_path, pulse_path, replace_xy):
 
 def _regrid_cont_pulse(ds_control, ds_pulse, ds_out):
     regridder = xe.Regridder(ds_control, ds_out, "bilinear")
+    attrs = ds_control.attrs
     ds_control = regridder(ds_control) 
-
+    ds_control.attrs = attrs
+    
     regridder = xe.Regridder(ds_pulse, ds_out, "bilinear")
+    attrs = ds_pulse.attrs
     ds_pulse = regridder(ds_pulse) 
+    ds_pulse.attrs = attrs
     
     return(ds_control, ds_pulse)
 
 
-def _calc_greens(ds_control, ds_pulse, variable, pulse_size = 100):
+def _calc_greens(ds_control, ds_pulse, variable, m, pulse_size = 100):
     
     #A = find_area(ds_control.isel(time = 0), lat_bound_nm = 'lat_bounds', lon_bound_nm = 'lon_bounds')
     G = (ds_pulse[variable] - ds_control[variable])/(pulse_size)
-    G = G.groupby('time.year').mean()
+    times = pd.date_range('2000', periods=len(G['time']), freq='MS')
+    weights = times.shift(1, 'MS') - times
+    weights = xr.DataArray(weights, [('time', G['time'].values)]).astype('float')
+    G =  (G * weights).groupby('time.year').sum('time')/weights.groupby('time.year').sum('time')
+    #select ten years in for two of the models
+    ten_years_in = 10 #in months
+    if m == 'ACCESS':
+        G = G.isel(year = slice(ten_years_in,len(G.year)))
+    if m == 'UKESM1':
+        G = G.isel(year = slice(ten_years_in,len(G.year)))
+    #G = G.groupby('time.year').mean()
     G.attrs = ds_pulse.attrs
     
     return(G)
@@ -77,19 +97,19 @@ def _calc_greens_anomaly(anom_control, anom_pulse, variable, pulse_size = 100):
 
 
 #full function
-def import_regrid_calc(control_path, pulse_path, ds_out, variable, pulse_size = 100,  replace_xy = True, regrid = True, anomaly = False):
+def import_regrid_calc(control_path, pulse_path, ds_out, variable, m, pulse_size = 100,  replace_xy = True, regrid = True, anomaly = False):
     '''Imports the control run and pulse run for a CMIP6 model run, combines them on the date the pulse starts
     Regrids it to the chosen grid size
     Calculates the Green's Function'''
     
-    ds_control, ds_pulse = _import_combine_pulse_control(control_path, pulse_path, replace_xy)
+    ds_control, ds_pulse = _import_combine_pulse_control(control_path, pulse_path, replace_xy, m)
     if regrid == True:
         ds_control, ds_pulse = _regrid_cont_pulse(ds_control, ds_pulse, ds_out)
     if anomaly == True:
         anom_control, anom_pulse = _calc_anomaly(ds_control, ds_pulse)
         anom_G = _calc_greens_anomaly(anom_control, anom_pulse, variable, pulse_size)
     else:
-        G = _calc_greens(ds_control, ds_pulse, variable, pulse_size)
+        G = _calc_greens(ds_control, ds_pulse, variable, m, pulse_size)
     if anomaly == True:
         return(anom_control, anom_pulse, anom_G)
     else:
@@ -100,8 +120,20 @@ def import_regrid_calc(control_path, pulse_path, ds_out, variable, pulse_size = 
 def _regrid_ds(ds_in, ds_out):
     regridder = xe.Regridder(ds_in, ds_out,  'bilinear', ignore_degenerate = True)
     ds_new = regridder(ds_in) 
+    ds_new.attrs = ds_in.attrs
     return(ds_new)
 
+
+### define our output grid size
+
+ds_out = xr.Dataset(
+    {
+        "lat": (["lat"], np.arange(-89.5, 90.5, 1.0)),
+        "lon": (["lon"], np.arange(0, 360, 1)),
+        "lat_b": (["lat_b"], np.arange(-90.,91.,1.0)),
+        "lon_b":(["lon_b"], np.arange(.5, 361.5, 1.0))
+    }
+)
 
 #### function to find area of a grid cell from lat/lon ####
 def find_area(ds, R = 6378.1):
@@ -204,3 +236,61 @@ def convolve_single_lev_mean(G, E, dt):
             )
         )
     return C
+
+################################# dataset dictionaries ###########################
+
+
+model_run_pulse_dict = {'UKESM1_r1':'UKESM1-0-LL_esm-pi-CO2pulse_r1i1p1f2*', 
+                        'MIROC':'MIROC-ES2L_esm-pi-CO2pulse_r1i1p1f2*', 
+                        'NORESM2':'NorESM2-LM_esm-pi-CO2pulse_r1i1p1f1*', 
+                  'ACCESS':'ACCESS-ESM1-5_esm-pi-CO2pulse_r1i1p1f1*',  
+                        'GFDL': 'GFDL-ESM4_esm-pi-CO2pulse_r1i1p1f1**',
+                       'CANESM5_r1p2':'CanESM5_esm-pi-CO2pulse_r1i1p2f1*',
+                       'CANESM5_r2p2':'CanESM5_esm-pi-CO2pulse_r2i1p2f1*',
+                       'CANESM5_r3p2':'CanESM5_esm-pi-CO2pulse_r3i1p2f1*'}
+
+model_run_cdr_pulse_dict = {'UKESM1_r1':'UKESM1-0-LL_esm-pi-cdr-pulse_r1i1p1f2*', 
+                        'MIROC':'MIROC-ES2L_esm-pi-cdr-pulse_r1i1p1f2*', 
+                        'NORESM2':'NorESM2-LM_esm-pi-cdr-pulse_r1i1p1f1*', 
+                  'ACCESS':'ACCESS-ESM1-5_esm-pi-cdr-pulse_r1i1p1f1*',  
+                        'GFDL': 'GFDL-ESM4_esm-pi-cdr-pulse_r1i1p1f1**',
+                       'CANESM5_r1p2':'CanESM5_esm-pi-cdr-pulse_r1i1p2f1*',
+                       'CANESM5_r2p2':'CanESM5_esm-pi-cdr-pulse_r2i1p2f1*',
+                       'CANESM5_r3p2':'CanESM5_esm-pi-cdr-pulse_r3i1p2f1*'}
+
+
+model_run_esm_picontrol_dict = {'UKESM1_r1':'UKESM1-0-LL_esm-piControl_r1i1p1f2*', 
+                          'MIROC':'MIROC-ES2L_esm-piControl_r1i1p1f2*', 
+                          'NORESM2':'NorESM2-LM_esm-piControl_r1i1p1f1*', 
+                  'ACCESS':'ACCESS-ESM1-5_esm-piControl_r1i1p1f1*', 
+                          'GFDL': 'GFDL-ESM4_esm-piControl_r1i1p1f1**',
+                         'CANESM5_r1p2':'CanESM5_esm-piControl_r1i1p2f1*',
+                          'CANESM5_r1p1':'CanESM5_esm-piControl_r1i1p1f1*',
+                         } ## for use with pulse run
+
+model_run_picontrol_dict = {'UKESM1_r1':'UKESM1-0-LL_piControl_r1i1p1f2*', 
+                          'MIROC':'MIROC-ES2L_piControl_r1i1p1f2*', 
+                          'NORESM2':'NorESM2-LM_piControl_r1i1p1f1*', 
+                  'ACCESS':'ACCESS-ESM1-5_piControl_r1i1p1f1*', 
+                          'GFDL': 'GFDL-ESM4_piControl_r1i1p1f1**',
+                         'CANESM5_r1p2':'CanESM5_piControl_r1i1p2f1*',
+                          'CANESM5_r1p1':'CanESM5_piControl_r1i1p1f1*',
+                         } ## for use with 1pct run
+
+
+model_run_1pct_dict = {'UKESM1_r1':'UKESM1-0-LL_1pctCO2_r1i1p1f2*',
+                       'UKESM1_r2':'UKESM1-0-LL_1pctCO2_r2i1p1f2*',
+                       'UKESM1_r3':'UKESM1-0-LL_1pctCO2_r3i1p1f2*',
+                       'UKESM1_r4':'UKESM1-0-LL_1pctCO2_r4i1p1f2*',
+                        'MIROC':'MIROC-ES2L_1pctCO2_r1i1p1f2*', 
+                        'NORESM2':'NorESM2-LM_1pctCO2_r1i1p1f1*', 
+                  'ACCESS':'ACCESS-ESM1-5_1pctCO2_r1i1p1f1*',  
+                        'GFDL': 'GFDL-ESM4_1pctCO2_r1i1p1f1**',
+                      'CANESM5_r1p2':'CanESM5_1pctCO2_r1i1p2f1*',
+                      'CANESM5_r2p2':'CanESM5_1pctCO2_r2i1p2f1*',
+                      'CANESM5_r3p2':'CanESM5_1pctCO2_r3i1p2f1*',
+                      'CANESM5_r1p1':'CanESM5_1pctCO2_r1i1p1f1*',
+                      'CANESM5_r2p1':'CanESM5_1pctCO2_r2i1p1f1*',
+                      'CANESM5_r3p1':'CanESM5_1pctCO2_r3i1p1f1*'}
+
+model_color = {'UKESM1_r1':'olive', 'UKESM1_r2':'brown', 'UKESM1_r3':'green', 'UKESM1_r4':'lightgreen', 'NORESM2':'blue', 'GFDL':'red', 'MIROC':'purple', 'ACCESS':'pink', 'CANESM5_r1p2':'orange', 'CANESM5_r2p2':'sienna', 'CANESM5_r3p2':'goldenrod'}
